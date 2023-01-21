2a3196cd9d4c2ed5db87edb09f7bec68df7fb847
Tobipeters <oluwatobilobapeterschem@gmail.com>
2023-01-21 09:12:43 +0100

    route to scroll up and alignmnet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,7 +15,11 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, {
+      scrollPositionRestoration: 'enabled',
+    }),
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
