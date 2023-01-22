df972d4756ba17f09858e433d45884e7cb6132da
Tobipeters <oluwatobilobapeterschem@gmail.com>
2023-01-22 20:06:13 +0100

    slider partially implemented

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,8 +7,15 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   @Input() title!: string;
+  @Input() isArrowBack: boolean = false;
+
+  @Output() goback: EventEmitter<any> = new EventEmitter();
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  handleBack() {
+    this.goback.emit();
+  }
 }
